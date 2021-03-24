66d94cbcb09d2b3cc0d54197ef0d3cb8e98a6406
dbayandarov@gmail.com <dbayandarov@gmail.com>
2021-03-24 15:55:40 -0700

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    first_name: {type: String, required: true, maxLength: 100},
+    family_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+    }
+);
+
+//getters and setters that are not persistent 
+//to MongoDB
+AuthorSchema
+.virtual('name')
+.get(function () {
+    return this.family_name + ", " + this.first_name;
+}); 
+
+AuthorSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+   let lifespan = 
+        this.date_of_birth ? 
+        DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)+"-"
+        : '...-';
+    lifespan +=
+        this.date_of_death ? 
+        DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)+")"
+        : '...'+")";
+
+    return lifespan;
+   //    sreturn DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
